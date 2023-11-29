cfea93fff5e50ce8524e833522c052234d2cee93
Josue V. Garrido S <jv.garrido99@gmail.com>
2023-11-29 01:14:16 -0300

    taks finished

--- problems/03-batch-timeouts.js
+++ problems/03-batch-timeouts.js
@@ -33,8 +33,26 @@ console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 ***********************************************************************/
 
 function batchTimeouts(callbacks, delays) {
-  // Your code here
+  let timeoutobjs = callbacks.map((element, index) => {
+    return setTimeout(element, delays[index]);
+  }); 
+  return timeoutobjs;
+  
 }
+
+const sayHello = () => console.log('hi');
+const sayGoodbye = () => console.log('bye');
+const shout = () => console.log('WHAT?');
+const tasks = [sayHello, sayGoodbye, shout];
+const delays = [500, 200, 900];
+
+const timeoutObjs = batchTimeouts(tasks, delays); 
+// should print: 
+//  'bye' after 200 ms
+//  'hi' after 500 ms
+//  'WHAT?' after 900 ms
+
+console.log(timeoutObjs); // [ Timeout {...},  Timeout {...}, Timeout {...} ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
